f7176644de0d2562fb95d4ee931eb7debd6b6b79
Luis Gomez <lgomez@gmail.com>
2016-10-06 22:17:47 -0700

    Cleaning up styles and adding additional control buttons with their respective methods

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router'
 import Socket from './socket';
-import API from './api';
 
 import './index.css';
 
@@ -29,25 +28,37 @@ class Debugger extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      socket: null
+      player: {
+        version: process.env.REACT_APP_VERSION
+      },
+      socket: null,
+      ping: null
     }
   }
   componentDidMount() {
-    ws.on('message', msg => {
-      this.setState({ socket: 'open', msg });
+    ws.on('message', payload => {
+      this.setState({
+        ping: {
+          timestamp: new Date(),
+          payload
+        }
+      });
     });
     ws.on('open', () => {
-      this.setState({ socket: 'open' });
+      this.setState({
+        socket: 'open'
+      });
     });
     ws.on('close', () => {
-      this.setState({ socket: 'closed' });
+      this.setState({
+        socket: 'closed'
+      });
     });
   }
   render() {
     return (
       <div className="debugger">
-        <div><strong>Version:</strong> {process.env.REACT_APP_VERSION}</div>
-        <div><strong>API:</strong> {API.VERSION}</div>
+        <div><strong>Version:</strong> {}</div>
         <pre>
           {JSON.stringify(this.state, null, 2)}
         </pre>
@@ -59,8 +70,13 @@ class Debugger extends Component {
 class Controls extends Component {
   constructor(props) {
     super(props)
+    this.browse.bind(this);
     this.play.bind(this);
     this.pause.bind(this);
+    this.volume.bind(this);
+  }
+  browse() {
+    ws.send('setState,browse');
   }
   play() {
     ws.send('setState,playing');
@@ -68,11 +84,16 @@ class Controls extends Component {
   pause() {
     ws.send('setState,paused');
   }
+  volume() {
+    ws.send('setState,volume');
+  }
   render() {
     return (
       <footer>
+        <Icon name="list" onClick={this.browse} />
         <Icon name="play_arrow" onClick={this.play} />
         <Icon name="pause" onClick={this.pause} />
+        <Icon name="volume_up" onClick={this.volume} />
       </footer>
     );
   }
