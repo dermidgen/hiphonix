76b556183e0a3b2102d9d3ca461c0c58a327c44a
Luis Gomez <lgomez@gmail.com>
2016-10-07 21:54:22 -0700

    Reading fixtures on request instead of on start and adding fallback to “unimplemented” fixture response so commands can be added from UI without breaking things

---
+++ test/fixtures/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+function fixtures(cmd) {
+  const file = path.join(__dirname, `${cmd}.json`);
+  let data = {};
+  try {
+    data = JSON.parse(fs.readFileSync(file, 'utf8'));
+  } catch (e) {
+    data = {
+      type: cmd,
+      data: {
+        unimplemented: true
+      }
+    };
+  }
+  return data;
+}
+
+module.exports = fixtures;
--- test/server.js
+++ test/server.js
@@ -2,18 +2,11 @@ const debug = require('debug')('hip:server');
 const util = require('util');
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ port: 8080 });
+const fixtures = require('./fixtures');
 
 const i = (obj, depth = 1) => {
   return '\n' + util.inspect(obj, { depth, colors: true })
 }
-
-const fixtures = {
-  STATE: require('./fixtures/STATE.json'),
-  MPD_API_GET_OUTPUTS: require('./fixtures/MPD_API_GET_OUTPUTS.json'),
-  MPD_API_GET_BROWSE: require('./fixtures/MPD_API_GET_BROWSE.json'),
-  MPD_API_SEARCH: require('./fixtures/MPD_API_SEARCH.json'),
-  NET_LIST: require('./fixtures/NET_LIST.json'),
-};
 
 debug('Initializing test server...');
 
@@ -22,23 +15,22 @@ wss.on('connection', connection => {
   debug('Client connection established.');
 
   const pinger = setInterval(() => {
-    connection.send(JSON.stringify(fixtures.STATE));
+    connection.send(JSON.stringify(fixtures('STATE')));
   }, 2000)
 
   connection.on('message', message => {
     const parts = message.split(',');
     const cmd = parts.shift();
     const args = parts;
-    const fixture = fixtures[cmd]
-    const response = JSON.stringify(fixture)
+    const data = fixtures(cmd);
+    const response = JSON.stringify(data)
 
     debug('Message', i({
       message,
       parts,
       cmd,
       args,
-      fixture,
-      response
+      data
     }, 3));
 
     try {
