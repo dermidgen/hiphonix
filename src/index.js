c1202b68fef0871cc676b78a826ebcd02ad9e2dd
Luis Gomez <lgomez@gmail.com>
2016-11-10 21:32:56 -0800

    Today's UI work

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router'
 // import moment from 'moment';
-import Socket from './socket';
+// import Socket from './socket';
 import './index.css';
 
 const injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
-const socket = new Socket();
-window.socket = socket;
+// const socket = new Socket();
+// window.socket = socket;
+
+document.addEventListener('gesturestart', function (e) {
+    e.preventDefault();
+});
 
 class Menu extends Component {
   cancel() {
@@ -43,15 +47,26 @@ class Menu extends Component {
 //   next track icon
 //   repeat all/one/none cycle icon
 class Controls extends Component {
+  toggleQueue(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    browserHistory.push('/queue');
+    console.log('Toggle Queue');
+  }
+  togglePlayback(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log('Toggle Playback');
+  }
   render() {
     return (
-      <footer>
-        <Link to="/queue">
-          <div className="cover"></div>
+      <footer onClick={ this.toggleQueue.bind(this) }>
+        <div className="cover"></div>
+        <div className="titles">
           <div className="title">title</div>
           <div className="subtitle">subtitle</div>
-        </Link>
-        <div className="play-pause"><i className="material-icons">play_arrow</i></div>
+        </div>
+        <div className="play-pause material-icons" onClick={ this.togglePlayback.bind(this) }>play_arrow</div>
         <div className="progress-bar"><div></div></div>
       </footer>
     )
@@ -59,17 +74,25 @@ class Controls extends Component {
 }
 
 class Header extends Component {
-  openSettings(event) {
-    event.preventDefault();
-    event.stopPropagation();
-  }
   render() {
     return (
       <header>
-        <Link id="left" onClick={browserHistory.goBack} className="material-icons">close</Link>
+        {(() => {
+          if (this.props.title !== "Library") {
+            return (
+              <Link id="left" onClick={browserHistory.goBack} className="material-icons">close</Link>
+            );
+          }
+        })()}
         <span id="title">{this.props.title}</span>
         <i id="volume" className="material-icons">volume_up</i>
-        <Link id="settings" to="/settings" className="material-icons">settings</Link>
+        {(() => {
+          if (this.props.title !== "Settings") {
+            return (
+              <Link id="settings" to="/settings" className="material-icons">settings</Link>
+            );
+          }
+        })()}
       </header>
     );
   }
@@ -102,10 +125,36 @@ class Settings extends Component {
 
 class Queue extends Component {
   render() {
+    var items = [
+      {
+        icon: 'drag_handle',
+        title: 'Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      }
+    ]
     return (
       <section>
         <Header title="Queue"/>
-        <List/>
+        <List items={ items }/>
         <Menu/>
       </section>
     )
@@ -116,22 +165,55 @@ class List extends Component {
   render() {
     return (
       <ul>
-        {[1,2,3].map(item => {
-          return <Item key={item} title={`Title ${item}`} subtitle={`Subtitle ${item}`} />;
+        {this.props.items.map((item, index) => {
+          return <Item key={index} {...item} />;
         })}
       </ul>
     )
   }
 }
 
+// icons:
+//  folder
+//  audiotrack
+//  playlist_play
+//  storage
+
+// actions:
+//  more_vert
+//  chevron_right
+
+// other
+//  repeat
+//  shuffle
+//  skip_previous
+//  skip_next
+//  arrow_back
+//  wifi
+//  volume_up
+//  close
+
 class Item extends Component {
+  handleClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log('Handling Click');
+  }
   render() {
     return (
-      <li>
-        <div className="icon"></div>
-        <div className="title">{this.props.title}</div>
-        <div className="subtitle">{this.props.subtitle}</div>
-        <a className="action material-icons">play_arrow</a>
+      <li onClick={ this.handleClick.bind(this) }>
+        <div className="icon material-icons">{this.props.icon}</div>
+        <div className="titles">
+          <div className="title">{this.props.title}</div>
+          {(() => {
+            if (this.props.subtitle) {
+              return (
+                <div className="subtitle">{this.props.subtitle}</div>
+              );
+            }
+          })()}
+        </div>
+        <a className="action material-icons">{this.props.action}</a>
       </li>
     )
   }
@@ -139,10 +221,72 @@ class Item extends Component {
 
 class Library extends Component {
   render() {
+    var items = [
+      {
+        icon: 'folder',
+        title: 'Folder',
+        subtitle: '3 items',
+        action: 'chevron_right'
+      },
+      {
+        icon: 'playlist_play',
+        title: 'Playlist',
+        subtitle: '20 items',
+        action: 'chevron_right'
+      },
+      {
+        icon: 'audiotrack',
+        title: 'Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Draggable Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Draggable Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Draggable Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Draggable Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Draggable Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Draggable Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      },
+      {
+        icon: 'drag_handle',
+        title: 'Draggable Song',
+        subtitle: '0:00',
+        action: 'play_arrow'
+      }
+    ]
     return (
       <section>
         <Header title="Library"/>
-        <List/>
+        <List items={ items }/>
         <Menu/>
       </section>
     )
