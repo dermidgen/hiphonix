f7f40bf21c6a41a8b1e4290d2c0455621456be79
Danny Graham <good.midget@gmail.com>
2016-11-16 14:33:07 -0800

    adding headers for gdbus stub

---
+++ src/gdbus/list-connections.h
@@ -0,0 +1,14 @@
+/*
+ * list-connections.h
+ *
+ *  Created on: Nov 14, 2016
+ *      Author: dgraham
+ */
+
+#ifndef SRC_GDBUS_LIST_CONNECTIONS_H_
+#define SRC_GDBUS_LIST_CONNECTIONS_H_
+
+
+void nm_list_connections();
+
+#endif /* SRC_GDBUS_LIST_CONNECTIONS_H_ */
